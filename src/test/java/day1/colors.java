f84a3d973ba2982d106947feb2738deef3420285
Gunel Khantamirova <gun040el@gmail.com>
2020-04-10 02:29:23 -0400

    Added print statment class

--- src/test/java/day1/colors.java
+++ src/test/java/day1/colors.java
@@ -3,7 +3,7 @@ package day1;
 public class colors {
     public static void main(String[] args) {
         String[] colors = {"red" , "blue" , "greeen" ,"yellow"};
-
+        System.out.println("my colors");
     }
 
 }
